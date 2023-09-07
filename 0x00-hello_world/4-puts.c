d592f54cf2c46618b7585eedbcc69dafd6cec215
chikizy <chijinduonyemelukwe001@gmail.com>
2023-09-07 16:39:39 -0700

    4-puts.c and 5-printf.c file added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main-refers to the main function
+ *
+ * Return: should be 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main-this function prints hello world
+ *
+ *Return:0 if good
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
