1aaa3bfc7f730ebaa9623967c095e996a12e1823
chikizy <chijinduonyemelukwe001@gmail.com>
2023-09-12 21:38:35 -0700

    added 0-putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_alphabet(void)-function to print alphabets
+* in lower case
+* Return:
+*/
+
+void print_alphabet(void);
+{
+	char c;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
