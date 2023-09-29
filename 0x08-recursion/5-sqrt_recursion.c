347f6757ee549cea165fbe527a18d1855a097bd5
chikizy <chijinduonyemelukwe001@gmail.com>
2023-09-29 05:30:33 +0100

    updated tasks 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -24,3 +24,21 @@ res = sqrt;
 sqrt = (n / res + res) / 2;
 return (_sqrt_recursion2(n, sqrt, res));
 }
+
+/**
+* _sqrt_recursion - fills memory with a constant byte.
+* @n: first bytes of the memory
+* Return: -
+*/
+int _sqrt_recursion(int n)
+{
+float res, sqrt;
+
+sqrt = n / 2;
+res = 0;
+if (n == 1)
+return (1);
+if (n < 0)
+return (-1);
+return (_sqrt_recursion2(n, sqrt, res));
+}
