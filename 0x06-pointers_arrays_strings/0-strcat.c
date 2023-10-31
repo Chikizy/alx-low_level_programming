42cfdc572ea67827c79510e2b888c204178b1d9f
chikizy <chijinduonyemelukwe001@gmail.com>
2023-10-31 11:56:34 +0100

    my strcat code

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,38 +1,24 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
- *
- * Return: void
+ *_strcat- function concatenates two strings
+ *@dest: first string
+ *@src: string concatenated to first
+ *Return: pointer to the updated string
  */
+
+char *_strcat(char *dest, char *src);
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int len1, len2, i;
 
-	i = 0;
-
-	while (dest[i] != '\0')
-
-	{
-		i++;
-	}
-
-	j = 0;
-
-	while (src[j] != '\0')
-	{
-
-	dest[i] = src[j];
-
-		i++;
-		j++;
-	}
-
-	dest[i] = '\0';
-
+	for (len1 = 0; dest[len1] != '\0' ; len1++)
+		;
+	for (len2 = 0; src[len2] != '\0'; len2++)
+		;
+	for (i = len1 ; i <= (len1 + len2) - 1 ; i++)
+		dest[i] = src[i - len1];
+	dest[len1 + len2] = '\0';
 	return (dest);
 }
