8602d51523374fd198446f97c1b139441f3ea2f7
chikizy <chijinduonyemelukwe001@gmail.com>
2024-01-18 15:48:49 +0100

    pre_main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * pre_main - function executed before main
- * Return: no return.
+ * premain - function that runs before main
+ *
+ * Return: nothing
  */
-
-void __attribute__ ((constructor)) pre_main()
+void __attribute__((constructor)) pre_main()
 {
-	printf("You're beat! and yet, you must allow");
-	printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
