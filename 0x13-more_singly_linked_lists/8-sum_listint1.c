712583760f0834edb42264157e5a33d06593f974
chikizy <chijinduonyemelukwe001@gmail.com>
2024-01-29 23:42:40 +0100

    task8

--- 0x13-more_singly_linked_lists/8-sum_listint1.c
+++ 0x13-more_singly_linked_lists/8-sum_listint1.c
@@ -9,16 +9,14 @@
  */
 int sum_listint(listint_t *head)
 {
-	int sum = 0;
-	listint_t *temp = head;
+	int sum;
 
-	if (head)
+	sum = 0;
+	while (head != NULL)
 	{
-		while (temp != NULL)
-			sum += temp->n;
-			temp = temp->next;
-		return (sum);
+		sum += head->n;
+		head = head->next;
 	}
-	else
-			return (0);
+
+	return (sum);
 }
